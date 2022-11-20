010a2cb92ead3d7fe7fff06374211afd2b43872f
Natan Augusto <natanaugusto@gmail.com>
2022-11-20 12:55:29 -0300

    Created chirper basic files (and added named arguments on web.php)
    ```
    sail artisan make:model -mrc Chirp
    ```
    
    - https://bootcamp.laravel.com/inertia/creating-chirps
    - https://www.php.net/manual/en/functions.arguments.php#functions.named-arguments

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -16,8 +17,8 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
+Route::get(uri:'/', action:function () {
+    return Inertia::render(component:'Welcome', props:[
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
@@ -25,14 +26,18 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+Route::get(uri:'/dashboard', action:function () {
+    return Inertia::render(component:'Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware('auth')->group(callback:function () {
+    Route::get(uri:'/profile', action:[ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch(uri:'/profile', action:[ProfileController::class, 'update'])->name('profile.update');
+    Route::delete(uri:'/profile', action:[ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource(name:'chirps', controller:ChirpController::class)
+    ->only(methods:['index', 'store'])
+    ->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth.php';
